246c61a890f5522cea385c992a9e543a82cfe951
Changzeng <1207338829@qq.com>
2018-04-12 23:47:42 +0800

    add buffer writer

---
+++ buffer_writer.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+# author: liaochangzeng
+# e-mail: 1207338829@qq.com
+
+
+class BufferWriter(object):
+	def __init__(self, file_name, max_buffer_size=30*1024*1024):
+		self.file_name = file_name
+		self.fd = open(self.file_name, "a+")
+		self.records = []
+		self.records_size = 0
+		self.max_buffer_size = max_buffer_size
+
+	def update(self, record):
+		self.records.append(record)
+		self.records_size += len(record)
+
+		if self.records_size >= self.max_buffer_size:
+			self.write_to_file()
+			self.records = []
+			self.records_size = 0
+
+	def write_to_file(self):
+		self.fd.write("\n".join(self.records))
+		self.fd.write("\n")
+
+	def close(self):
+		self.fd.close()
